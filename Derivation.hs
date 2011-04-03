c74f384229382d3b75c0b3359854844209fbf272
David Mortensen <davidmortensen@gmail.com>
2011-04-03 15:33:54 -0400

    Added main functionality to Derivation.hs

--- Derivation.hs
+++ Derivation.hs
@@ -4,11 +4,29 @@ import Data.Phonology.Representations
 import Data.Phonology.Rules
 import Data.Phonology.RuleParsers
 
-import Network.CGI
+import Data.List (transpose)
+import Data.Maybe (fromMaybe)
 
-toExandedRules :: RuleState -> String -> [[Rule]]
-toExandedRules st = map (readRuleV st) . lines
+import Network.CGI
 
 toRepresentations :: RuleState -> String -> [[Segment]]
 toRepresentations st = map (readIPA st) . lines
 
+maybeDerivationV' :: RuleState -> String -> String -> [[Maybe String]]
+maybeDerivationV' st ruleText = transpose . 
+                                map (flip (maybeDerivationV (readRuleV st)) (lines ruleText)) . 
+                                toRepresentations st
+
+formatTable :: [[Maybe String]] -> String
+formatTable = ("<table>"++) . (++"</table") . 
+              concatMap (("<tr>"++) . (++"</tr>") . 
+                         concatMap (("<td>"++) . (++"</td>") . fromMaybe "---" ))
+
+cgiMain :: CGI CGIResult
+cgiMain = setHeader "Content-type" "text/html; charset=UTF-8" >> 
+          getInput "ruletext" >>= 
+          \(Just rs) -> (getInput "reptext" >>= 
+                         output . formatTable . maybeDerivationV' defState rs . fromMaybe "")
+
+main :: IO ()
+main = runCGI (handleErrors cgiMain)
