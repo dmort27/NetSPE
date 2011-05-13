4f42cb2fe8ae9ca4156c29b362c485e328a3ba62
David Mortensen <davidmortensen@gmail.com>
2011-05-13 21:30:27 +0200

    Fixed install script.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo mkdir -p /Library/WebServer/CGI-Executables/netspe
 sudo mkdir /Library/WebServer/Documents/netspe
 sudo cp dist/build/derivation.cgi/derivation.cgi /Library/WebServer/CGI-Executables/netspe/
-sudo cp dist/html /Library/WebServer/Documents/netspe/
+sudo cp -r netspe/* /Library/WebServer/Documents/netspe/
