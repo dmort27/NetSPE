2daacd0815c0128c23d327ff8aa6b5c466278625
David Mortensen <davidmortensen@gmail.com>
2011-05-16 15:07:05 +0200

    Updated install script and netspe.js.

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 sudo mkdir -p /Library/WebServer/CGI-Executables/netspe
-sudo mkdir /Library/WebServer/Documents/netspe
+sudo mkdir -p /Library/WebServer/Documents/netspe
 sudo cp dist/build/derivation.cgi/derivation.cgi /Library/WebServer/CGI-Executables/netspe/
 sudo cp -r netspe/* /Library/WebServer/Documents/netspe/
--- netspe/js/netspe.js
+++ netspe/js/netspe.js
@@ -3,7 +3,7 @@ $(document).ready( function() {
 var evaluate = function() {
     var dt = { ruletext: $('#ruletext').val(),
 	       reptext: $('#reptext').val() };
-    $('#derivation').load('/cgi-bin/netspe/derivation.cgi');
+    $('#derivation').load('/cgi-bin/netspe/derivation.cgi', dt);
 };
 
 $('#evaluate').button();
