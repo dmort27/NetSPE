609aa052bec276c73f1c1cf5700917740f1b664d
David Mortensen <davidmortensen@gmail.com>
2011-05-23 14:14:57 +0200

    Improved interface.

--- netspe/js/netspe.js
+++ netspe/js/netspe.js
@@ -1,12 +1,28 @@
 $(document).ready( function() {
 
-var evaluate = function() {
-    var dt = { ruletext: $('#ruletext').val(),
-	       reptext: $('#reptext').val() };
-    $('#derivation').load('/cgi-bin/netspe/derivation.cgi', dt);
-};
+    var enclose = function(l, r, s) {
+        return l + s + r;
+    };
+    
+    var formatDerivation = function() {
+        $("tr:first").addClass("ur");
+        $("tr:last").addClass("sr");
+        $("tr.ur td").each(function(i) {
+            $(this).text(enclose('/', '/', $(this).text()));
+        });
 
-$('#evaluate').button();
-$('#evaluate').click(evaluate);
-
+        $("tr.sr td").each(function(i) {
+            $(this).text(enclose('[', ']', $(this).text()));
+        });
+    };
+    
+    var evaluate = function() {
+        var dt = { ruletext: $('#ruletext').val(),
+	           reptext: $('#reptext').val() };
+        $('#derivation').load('/cgi-bin/netspe/derivation.cgi', dt, function(){ formatDerivation(); });
+    };
+    
+    $('#evaluate').button();
+    $('#evaluate').click(evaluate);
+    
 });
