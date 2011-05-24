0ac62f919c7be4eec8d2cbc24c3d8586e1f96cc6
David Mortensen <davidmortensen@gmail.com>
2011-05-24 22:09:29 +0200

    Numerous UI changes and enhancements.

--- netspe/js/netspe.js
+++ netspe/js/netspe.js
@@ -1,8 +1,65 @@
+var enclose = function(l, r, s) {
+    return l + s + r;
+};
+
+// var lenSharedPrefix = function(s1, s2) {
+//     var i, result;
+//     for (i=0; i < s1.length; i++) {
+//         if (s1[i] == s2[i]) {
+//             result = i + 1;
+//         }
+//     }
+//     return result;
+// };
+
+// var lenSharedSuffix = function(s1, s2) {
+//     var t1 = s1.split('').reverse().join('');
+//     var t2 = s2.split('').reverse().join('');
+//     var len = lenSharedPrefix(t1, t2);
+
+//     if (len === undefined) {
+//         return -1;
+//     } else {
+//         return len;
+//     }
+// };
+
+// var parts = function(s1, s2) {
+//     var parts;
+//     if (s1 === s2) {
+//         return [s2, "", ""];
+//     } else {
+//         var pref = lenSharedPrefix(s1, s2);
+//         var suff = lenSharedSuffix(s1, s2);
+//         if (suff === -1 && pref == -1) {
+//             return ["", s2, ""];
+//         } else {
+//             if (suff === -1) {
+//                 return [s2.slice(0, pref), s2.slice(pref + 1), ""];
+//             } else {
+//                 return [];
+//             }
+//         }
+//     }
+// };
+
+var textareaToSortableList = function(id) {
+    var ruleList = $(id).val().split('\n')
+        .map(function(r) {
+            return '<li class="phonetic">' + r + '</li>';
+        }).join('');
+    $(id).replaceWith('<ul id="ruletext" class="sortable">' + ruleList + '<ul>');
+    $(".sortable").sortable();
+    $(".sortable").disableSelection();
+    $(".sortable li").addClass("ui-state-default");
+};
+
 $(document).ready( function() {
 
-    var enclose = function(l, r, s) {
-        return l + s + r;
-    };
+    $("#controls").addClass("ui-widget phonetic");
+    $(".box").addClass("ui-widget phonetic");
+    $(".box textarea").addClass("ui-widget phonetic");
+    $("h2").addClass("ui-widget ui-widget-header phonetic");
     
     var formatDerivation = function() {
         $("tr:first").addClass("ur");
