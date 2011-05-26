427ee3124dd7c784bd9e432d89d79d7f7eaff6bd
David Mortensen <davidmortensen@gmail.com>
2011-05-26 12:05:31 +0200

    Generalized installation script.

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
-sudo mkdir -p /Library/WebServer/CGI-Executables/netspe
-sudo mkdir -p /Library/WebServer/Documents/netspe
-sudo cp dist/build/derivation.cgi/derivation.cgi /Library/WebServer/CGI-Executables/netspe/
-sudo cp -r netspe/* /Library/WebServer/Documents/netspe/
+export WEBROOT=/Library/WebServer/Documents/
+export CGIROOT=/Library/WebServer/CGI-Executables/
+
+sudo mkdir -p $CGIROOT"netspe"
+sudo mkdir -p $DOCROOT"netspe"
+sudo cp dist/build/derivation.cgi/derivation.cgi $CGIROOT"netspe/"
+sudo cp -r netspe/* $WEBROOT"netspe/"
+sudo chmod -Rv 755 $WEBROOT"netspe/"
