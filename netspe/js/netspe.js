9d9a9d223f9a8deb1b4a97b16385d5496b22e63a
David Mortensen <davidmortensen@gmail.com>
2011-07-12 19:06:48 +0200

    Added comments and began refactoring.

--- netspe/js/netspe.js
+++ netspe/js/netspe.js
@@ -8,6 +8,7 @@ var unenclose = function(x) {
     return x.slice(1, x.length - 1);
 };
 
+// Add word boundary markers (#) to all forms in a set.
 var mapAddBoundaries = function(xs) {
     return $.map(xs, function(v, i) {
         var match = /([^ ]+)( +['‘].+['’])/.exec(v);
@@ -19,6 +20,8 @@ var mapAddBoundaries = function(xs) {
     });
 };
 
+// Parses a form/gloss pair and returns an object with the fields form
+// and gloss.
 var parseFormGloss = function(x) {
     var match = /([^ ]+)(\s+['‘].+['’]|)/.exec(x);
     var result = new Object;
@@ -27,11 +30,8 @@ var parseFormGloss = function(x) {
     return result;
 };
 
-var phoneticFonts = "FreeSans, Lucida Grande, DejaVu Sans, Menlo, Helvetica, Arial, Stone Sans Sem ITC," +
-                    "Charis SIL, Gentium Plus, STIXGeneral, Doulos SIL, Monaco, Times New Roman," +
-                    "DejaVu Serif, sans, serif";
 
-// converts a textarea control into a sortable list.
+// Converts a textarea control into a sortable list.
 var textareaToSortableList = function(id) {
 
     var newList = $('<ul />').attr('id', id).addClass('sortable-parent');
@@ -48,7 +48,7 @@ var textareaToSortableList = function(id) {
 
 };
 
-// converts a sortable list into a textarea control.
+// Converts a sortable list into a textarea control.
 var sortableListToTextarea = function(id) {
     var ruleList = [];
     var qid = '#' + id;
@@ -63,69 +63,67 @@ var sortableListToTextarea = function(id) {
     $(qid).addClass('phonetic');;
 };
 
+// This function is called to format the derivation after it has
+// been loaded from the server. 
+var formatDerivation = function() {
+    $("tr:first").addClass("ur ui-priority-primary");
+    $("tr:last").addClass("sr ui-priority-primary");
+    $("tr.ur td").each(function(i) {
+        $(this).text(enclose('/', '/', $(this).text()));
+    });
+
+    $("tr.sr td").each(function(i) {
+        $(this).text(enclose('[', ']', $(this).text()));
+    });
+
+    $("td").each(function(i) {
+        if ($(this).text()==='---') { $(this).text('—'); }
+    });
+    
+    // Build a row of intended SRs.
+    
+    var srepsTR = $('<tr/>');
+    var srepsCalc = $('#main table tr:last-child')
+        .children().map( function() {
+            return unenclose($(this).text());
+        });
+
+    $.each($('#sreptext').val().split('\n'), function(i, v) {
+        // Applies formatting if the calculated form is different
+        // from the expected value.
+        if (v.replace(/[-]/gi, '').replace(/\s+.+/gi, '') === srepsCalc[i]) { // remove any hyphens and glosses
+            srepsTR.append($('<td></td>').append(v));
+        } else {
+            srepsTR.append($('<td></td>').append($('<b></b>').append(v)));
+        }
+    });
+    $('#main table').append(srepsTR).addClass('phonetic');
+
+    $('#main table tr:last').css({'border-bottom': '2px solid black'});
+    
+    // Get rules (with fillers).
+    var rules = ["UR"].concat($('#ruletext').val().split('\n'), ["SR (predicted)", "SR (observed)"]);
+
+    // Append rule to each table row
+    $('#derivation-container tr').each( function(i, tr) {
+        $(tr).append($('<td />').text(rules[i]));
+    });
+    $('#derivation table tr').addClass('phonetic derivation');
+    $('#derivation table').addClass('ui-widget phonetic derivation');
+    $('#derivation td').addClass('derivation');
+
+};
+
+// Main function.
 $(document).ready( function() {
 
     // Applies classes and styles to various parts of the document.
-    // $('textarea').css('font-family', phoneticFonts)
-    //     .css('font-size', '0.8em');
     $('textarea').addClass('phonetic')
         .css({'font-size': '0.9em'});
     $("#controls").addClass("ui-widget");
     $(".control textarea").addClass("ui-widget ui-helper-clearfix")
         .css('padding', '2px 2px 2px 2px');
     $("h2").addClass("ui-widget ui-widget-header ui-corner-tl ui-corner-tr fancy");
-    
-    // This function is called to format the derivation after it has
-        // been loaded from the server. 
-    var formatDerivation = function() {
-        $("tr:first").addClass("ur ui-priority-primary");
-        $("tr:last").addClass("sr ui-priority-primary");
-        $("tr.ur td").each(function(i) {
-            $(this).text(enclose('/', '/', $(this).text()));
-        });
-
-        $("tr.sr td").each(function(i) {
-            $(this).text(enclose('[', ']', $(this).text()));
-        });
-
-        $("td").each(function(i) {
-            if ($(this).text()==='---') { $(this).text('—'); }
-        });
-        
-        $('#derivation table').addClass("ui-widget phonetic")
-            .css('font-size', '0.8em');
-
-        // Build a row of intended SRs.
-            
-        var srepsTR = $('<tr/>');
-
-        var srepsCalc = $('#main table tr:last-child')
-            .children().map( function() {
-                return unenclose($(this).text());
-            });
-
-        $.each($('#sreptext').val().split('\n'), function(i, v) {
-            // Applies formatting if the calculated form is different
-            // from the expected value.
-            if (v.replace(/[-]/gi, '').replace(/\s+.+/gi, '') === srepsCalc[i]) { // remove any hyphens and glosses
-                srepsTR.append($('<td></td>').append(v));
-            } else {
-                srepsTR.append($('<td></td>').append($('<b></b>').append(v)));
-            }
-        });
-        $('#main table').append(srepsTR).addClass('phonetic');
-
-        $('#main table tr:last').css({'border-bottom': '2px solid black'});
-
-        // Get rules (with fillers).
-        var rules = ["UR"].concat($('#ruletext').val().split('\n'), ["SR (predicted)", "SR (observed)"]);
-
-        // Append rule to each table row
-        $('#derivation-container tr').each( function(i, tr) {
-            $(tr).append($('<td />').text(rules[i]));
-        });
-        $('#main table tr').addClass('phonetic');
-    };
     
     var evaluate = function() {
 
@@ -138,7 +136,7 @@ $(document).ready( function() {
                  };
         $('#derivation-container')
             .load('/cgi-bin/netspe/derivation.cgi', dt,
-                  function(){
+                  function() {
                       formatDerivation();
                   });
     };
