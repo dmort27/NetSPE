e521f5a41d312215eb5586d119b4450f257a3164
David Mortensen <davidmortensen@gmail.com>
2012-01-22 21:00:34 -0500

    Corrected bug in paths.

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ export WEBROOT=/Library/WebServer/Documents/
 export CGIROOT=/Library/WebServer/CGI-Executables/
 
 mkdir -p $CGIROOT"netspe"
-mkdir -p $DOCROOT"netspe"
+mkdir -p $WEBROOT"netspe"
 cp dist/build/derivation.cgi/derivation.cgi $CGIROOT"netspe/"
 cp dist/build/lint.cgi/lint.cgi $CGIROOT"netspe/"
 cp -r netspe/* $WEBROOT"netspe/"
